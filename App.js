9c9a3f6ae09fa07648aa20f2e4cd1d31673acf5f
victgab20 <capic.2022118tads0021@aluno.ifpi.edu.br>
2024-08-22 11:01:44 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Button, FlatList, TextInput } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as SQLite from 'expo-sqlite';
+
+
+async function openDatabase(item){
+
+const db = await SQLite.openDatabaseAsync('database');
+
+await db.execAsync(`
+  PRAGMA journal_mode = WAL;
+  CREATE TABLE IF NOT EXISTS item (id INTEGER PRIMARY KEY NOT NULL, value VARCHAR(255) NOT NULL);
+  `);
+  const result = await db.runAsync('INSERT INTO item (value) VALUES (?)', item);
+  const allRows = await db.getAllAsync('SELECT * FROM item');
+  console.log(allRows)
+  return allRows;
+}
+
+
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  const [texInput, setTextInput] = useState('')
+
+  
+  const handlePress = async () => {
+    const rows = await openDatabase(texInput);
+    console.log(texInput);
+    setTextInput('');
+    setItems(rows)
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Exemplo de SQLite com Expo</Text>
+      <TextInput
+        placeholder="Digite um pokemon"
+        value={texInput}
+        onChangeText={setTextInput}
+        style={styles.input}
+      />
+      <Button title="ADICIONAR"
+      onPress={handlePress}/>
+      <View style={{height: '40%', width: '40%'}}>
+        <FlatList
+          data={items}
+          style={{
+            borderBlockColor: 'red'
+          }}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.box}>
+              <Text style={{width: '30px'}}>{item.value}</Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +64,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 3
   },
+  title: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  box:{
+    fontSize: 20,
+    borderWidth: 2,
+    padding: '5%',
+    width: '80px',
+  },
+  input: {
+    borderWidth: 2,
+    height: '5%',
+    width: '50%'
+  }
 });
